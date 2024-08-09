8a33ad24cbcbfc3e5ab2b5ff0ff50a6db44b448d
emreeilhan <emreilhn15@gmail.com>
2024-08-09 11:30:29 +0300

    Initial Commit

---
+++ QRCodeGenerator/QRCodeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeGeneratorApp.swift
+//  QRCodeGenerator
+//
+//  Created by Emre Ilhan on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
